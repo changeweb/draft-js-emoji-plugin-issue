bee55f149800175208b9f2a56e71767578c98270
Hasib Mahmud <kishorpasha870@yahoo.com>
2016-07-07 12:36:26 +0600

    Create MyEditor.js

---
+++ src/MyEditor.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Editor from 'draft-js-plugins-editor';
+import createEmojiPlugin from 'draft-js-emoji-plugin';
+import { EditorState } from 'draft-js';
+import 'draft-js-emoji-plugin/lib/plugin.css';
+
+const emojiPlugin = createEmojiPlugin();
+const { EmojiSuggestions } = emojiPlugin;
+
+const plugins = [
+  emojiPlugin,
+];
+
+export default class GoroEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    };
+
+    this.onChange = (editorState) => {
+      this.setState({
+        editorState
+      });
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Editor
+          editorState={this.state.editorState}
+          onChange={this.onChange}
+          plugins={plugins}
+          ref="editor"
+        />
+        <EmojiSuggestions />
+      </div>
+    );
+  }
+}
